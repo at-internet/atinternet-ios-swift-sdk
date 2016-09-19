13e647886673609a692a66c2484bfa3702772137
nsagnette <nicolas.sagnette@atinternet.com>
2016-09-19 12:06:35 +0200

    Swift 2.3 compatibility

---
+++ README.md
@@ -0,0 +1,72 @@
+# AT Internet iOS SDK
+The AT Internet tag allows you to follow your users activity throughout your application’s lifecycle.
+To help you, the tag makes available classes (helpers) enabling the quick implementation of tracking for different application events (screen loads, gestures, video plays…)
+
+### Content
+* Tag iPhone / iPad
+* App Extension supported
+
+### How to get started
+  - Install our library on your project (see below)
+  - Check out the [documentation page] for an overview of the functionalities and code examples
+
+### Manual integration
+Find the integration information by following [this link]
+
+###Installation with CocoaPods
+
+CocoaPods is a dependency manager which automates and simplifies the process of using 3rd-party libraries in your projects.
+
+###Podfile
+
+  - Basic iOS application : 
+
+```ruby
+target 'MyProject' do
+pod "ATInternet-iOS-Swift-SDK/iOS",">=2.0"
+use_frameworks!
+end
+```
+
+  - iOS application with App Extension : 
+
+```ruby
+# required when building for App Extension
+pod "ATInternet-iOS-Swift-SDK",">=2.0"
+use_frameworks!
+
+target 'MyProject' do
+
+pod "ATInternet-iOS-Swift-SDK/iOS",">=2.0"
+use_frameworks!
+
+end
+
+target 'MyProject App Extension' do
+
+pod "ATInternet-iOS-Swift-SDK/AppExtension",">=2.0"
+use_frameworks!
+
+end
+```
+
+###Installation with Carthage
+
+Carthage is an alternative to **Cocoapods**. It’s a simple dependency manager for Mac and iOS, created by a group of developers from Github.
+
+###Cartfile
+Simplty add the line below :
+
+```
+github "at-internet/atinternet-ios-swift-sdk" >=2.0
+```
+- After launching the **carthage update** command, add the dependency from Project/Carthage/Build/iOS/Tracker.framework in **Linked Frameworks and Libraries**
+- In **project build phase**, add a **new run script phase** with the command /usr/local/bin/carthage copy-frameworks and add $(SRCROOT)/Carthage/Build/iOS/Tracker.framework as **input files**
+- Finally **project build phase**, add a **new copy files**, select **products directory** then add the Tracker.framework.dSYM file
+
+### License
+MIT
+
+
+   [this link]: <http://developers.atinternet-solutions.com/ios-en/getting-started-en/integration-of-the-swift-library-ios-en/>
+   [documentation page]: <http://developers.atinternet-solutions.com/ios-en/getting-started-en/integration-of-the-swift-library-ios-en/>
---
+++ Tracker/Tracker/Screen.swift
@@ -0,0 +1,380 @@
+/*
+This SDK is licensed under the MIT license (MIT)
+Copyright (c) 2015- Applied Technologies Internet SAS (registration number B 403 261 258 - Trade and Companies Register of Bordeaux – France)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+
+
+
+//
+//  Screen.swift
+//  Tracker
+//
+
+import UIKit
+
+public class ScreenInfo: BusinessObject {
+    
+}
+
+public class AbstractScreen: BusinessObject {
+    
+    /// Actions
+    public enum Action: String {
+        case View = "view"
+    }
+    
+    /// Touch name
+    public var name: String = ""
+    /// First chapter
+    public var chapter1: String?
+    /// Second chapter
+    public var chapter2: String?
+    /// Third chapter
+    public var chapter3: String?
+    /// Action
+    public var action: Action = Action.View
+    /// Level 2
+    public var level2: Int?
+    /// Screen contains cart info
+    public var isBasketScreen: Bool = false
+    
+    /// Set parameters in buffer
+    override func setEvent() {
+        if let optLevel2 = level2 {
+            self.tracker.setParam("s2", value: optLevel2)
+        }
+        
+        if (isBasketScreen) {
+            tracker.setParam("tp", value: "cart")
+        }
+    }
+    
+    /**
+    Send a screen view event
+    */
+    public func sendView() {
+        self.tracker.dispatcher.dispatch([self])
+    }
+}
+
+public class Screen: AbstractScreen {
+    //MARK: Screen
+    /// Set parameters in buffer
+    override func setEvent() {
+        super.setEvent()
+        
+        tracker.event.set("screen", action: action.rawValue, label: buildScreenName())
+    }
+    
+    //MARK: Screen name building
+    func buildScreenName() -> String {
+        var screenName = chapter1 == nil ? "" : chapter1! + "::"
+        screenName = chapter2 ==  nil ? screenName : screenName + chapter2! + "::"
+        screenName = chapter3 ==  nil ? screenName : screenName + chapter3! + "::"
+        screenName += name
+        
+        return screenName
+    }
+}
+
+public class DynamicScreen: AbstractScreen {
+    
+    /// Dynamic screen identifier
+    public var screenId: String = ""
+    /// Dynamic screen update date
+    public var update: NSDate = NSDate()
+    
+    let dateFormatter: NSDateFormatter = NSDateFormatter()
+    
+    /// Set parameters in buffer
+    override func setEvent() {
+        super.setEvent()
+        
+        let chapters = buildChapters()
+        
+        let encodingOption = ParamOption()
+        encodingOption.encode = true
+        
+        tracker.setParam("pchap", value: chapters == nil ? "" : chapters!, options:encodingOption)
+        
+        if(screenId.characters.count > 255){
+            screenId = ""
+            tracker.delegate?.warningDidOccur("screenId too long, replaced by empty value")
+        }
+        tracker.setParam("pid", value: screenId)
+        
+        dateFormatter.dateFormat = "YYYYMMddHHmm"
+        dateFormatter.locale = LifeCycle.locale
+        
+        tracker.setParam("pidt", value: dateFormatter.stringFromDate(update))
+        tracker.event.set("screen", action: action.rawValue, label: name)
+    }
+    
+    //MARK: Chapters building
+    func buildChapters() -> String? {
+        var value: String?
+        
+        if let optChapter1 = chapter1 {
+            value = optChapter1
+        }
+        
+        if let optChapter2 = chapter2 {
+            if(value == nil) {
+                value = optChapter2
+            } else {
+                value! += "::" + optChapter2
+            }
+        }
+        
+        if let optChapter3 = chapter3 {
+            if(value == nil) {
+                value = optChapter3
+            } else {
+                value! += "::" + optChapter3
+            }
+        }
+        
+        return value
+    }
+}
+
+public class Screens {
+    /// Tracker instance
+    var tracker: Tracker
+    
+    /**
+    Screens initializer
+    - parameter tracker: the tracker instance
+    - returns: Screens instance
+    */
+    init(tracker: Tracker) {
+        self.tracker = tracker
+    }
+    
+    /**
+    Set a screen
+    - returns: tracker instance
+    */
+    public func add() -> Screen {
+        let screen = Screen(tracker: tracker)
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    /**
+    Set a screen
+    - parameter screen: name
+    - returns: Screen instance
+    */
+    public func add(name:String) -> Screen {
+        let screen = Screen(tracker: tracker)
+        screen.name = name
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    /**
+    Set a screen
+    - parameter screen: name
+    - parameter first: chapter
+    - returns: Screen instance
+    */
+    public func add(name: String, chapter1: String) -> Screen {
+        let screen = Screen(tracker: tracker)
+        screen.name = name
+        screen.chapter1 = chapter1
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    /**
+    Set a screen
+    - parameter screen: name
+    - parameter first: chapter
+    - parameter second: chapter
+    - returns: Screen instance
+    */
+    public func add(name: String, chapter1: String, chapter2: String) -> Screen {
+        let screen = Screen(tracker: tracker)
+        screen.name = name
+        screen.chapter1 = chapter1
+        screen.chapter2 = chapter2
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    /**
+    Set a screen
+    - parameter screen: name
+    - parameter first: chapter
+    - parameter second: chapter
+    - parameter third: chapter
+    - returns: Screen instance
+    */
+    public func add(name: String, chapter1: String, chapter2: String, chapter3: String) -> Screen {
+        let screen = Screen(tracker: tracker)
+        screen.name = name
+        screen.chapter1 = chapter1
+        screen.chapter2 = chapter2
+        screen.chapter3 = chapter3
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+}
+
+public class DynamicScreens {
+    /// Tracker instance
+    var tracker: Tracker
+    
+    /**
+    DynamicScreens initializer
+    - parameter tracker: the tracker instance
+    - returns: DynamicScreens instance
+    */
+    init(tracker: Tracker) {
+        self.tracker = tracker
+    }
+    
+    @available(*, deprecated=2.2.1, message="Use add(screenId: String, update: NSDate, name: String) instead.")
+    public func add(screenId: Int, update: NSDate, name: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = String(screenId)
+        screen.update = update
+        screen.name = name
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    public func add(screenId: String, update: NSDate, name: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = screenId
+        screen.update = update
+        screen.name = name
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+
+    
+    /**
+    Set a dynamic screen
+    - parameter screen: name
+    - parameter first: chapter
+    - returns: DynamicScreen instance
+    */
+    @available(*, deprecated=2.2.1, message="Use add(screenId: String, update: NSDate, name: String, chapter1: String) instead.")
+    public func add(screenId: Int, update: NSDate,name: String, chapter1: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = String(screenId)
+        screen.update = update
+        screen.name = name
+        screen.chapter1 = chapter1
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    public func add(screenId: String, update: NSDate,name: String, chapter1: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = screenId
+        screen.update = update
+        screen.name = name
+        screen.chapter1 = chapter1
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    /**
+    Set a dynamic screen
+    - parameter screen: name
+    - parameter first: chapter
+    - parameter second: chapter
+    - returns: DynamicScreen instance
+    */
+    @available(*, deprecated=2.2.1, message="Use add(screenId: String, update: NSDate, name: String, chapter1: String, chapter2: String) instead.")
+    public func add(screenId: Int, update: NSDate,name: String, chapter1: String, chapter2: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = String(screenId)
+        screen.update = update
+        screen.name = name
+        screen.chapter1 = chapter1
+        screen.chapter2 = chapter2
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    public func add(screenId: String, update: NSDate,name: String, chapter1: String, chapter2: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = screenId
+        screen.update = update
+        screen.name = name
+        screen.chapter1 = chapter1
+        screen.chapter2 = chapter2
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    /**
+    Set a dynamic screen
+    - parameter screen: name
+    - parameter first: chapter
+    - parameter second: chapter
+    - parameter third: chapter
+    - returns: DynamicScreen instance
+    */
+    @available(*, deprecated=2.2.1, message="Use add(screenId: String, update: NSDate, name: String, chapter1: String, chapter2: String, chapter3: String) instead.")
+    public func add(screenId: Int, update: NSDate,name: String, chapter1: String, chapter2: String, chapter3: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = String(screenId)
+        screen.update = update
+        screen.name = name
+        screen.chapter1 = chapter1
+        screen.chapter2 = chapter2
+        screen.chapter3 = chapter3
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+    
+    public func add(screenId: String, update: NSDate,name: String, chapter1: String, chapter2: String, chapter3: String) -> DynamicScreen {
+        let screen = DynamicScreen(tracker: tracker)
+        screen.screenId = screenId
+        screen.update = update
+        screen.name = name
+        screen.chapter1 = chapter1
+        screen.chapter2 = chapter2
+        screen.chapter3 = chapter3
+        tracker.businessObjects[screen.id] = screen
+        
+        return screen
+    }
+}
